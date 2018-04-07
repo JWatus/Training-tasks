d86b1e468b9348a22afaafab89894319d84be0a8
Kuba <kwatus@gmail.com>
2018-04-07 10:33:52 +0200

    Dot counter task added

---
+++ src/com/company/dot_counter/Main.java
@@ -0,0 +1,12 @@
+package com.company.dot_counter;
+
+import com.company.bubble_sorting.BubbleSorting;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        DotCounter.countDots("Kji..s..d.s..f....s...");
+        DotCounter.compareLength("..Kji..s..d.s..f....s.....");
+    }
+}
