9d03bc43ae9ce030e722322fbbebe693c6c31e03
Kuba <kwatus@gmail.com>
2018-04-10 19:21:06 +0200

    Swaper task added

---
+++ src/com/company/swaper/Main.java
@@ -0,0 +1,11 @@
+package com.company.swaper;
+
+import com.company.string_palindrom.PalindromChecker;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Swaper.swapNumbers(1,8);
+    }
+}
