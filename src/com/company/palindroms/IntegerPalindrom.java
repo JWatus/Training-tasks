0c1b592c7e33aa6e72e3fb6f545ca5b47ad8f445
Kuba <kwatus@gmail.com>
2018-06-10 19:32:11 +0200

    Refactored few tasks

--- src/com/company/palindroms/IntegerPalindrom.java
+++ src/com/company/palindroms/IntegerPalindrom.java
@@ -2,7 +2,8 @@ package com.company.palindroms;
 
 public class IntegerPalindrom {
 
-    private boolean isPalindrom(Integer number) {  //121
+    public boolean isPalindrom(Integer number) {  //121
+
         Integer temp = number;
         Integer result = 0;
 
@@ -11,23 +12,12 @@ public class IntegerPalindrom {
             result = result * 10 + remain;  // 1        121
             temp /= 10;                     // 12       1
         }
+
         if (number.equals(result)) {
             return true;
         }
+
         return false;
-    }
-
-    private static boolean isIntegerPalindromReversed(Integer integer) {
-
-        boolean isPal = false;
-
-        StringBuilder stringBuilder = new StringBuilder(integer.toString());
-        stringBuilder.reverse();
-
-        if (integer.equals(Integer.valueOf(stringBuilder.toString()))) {
-            isPal = true;
-        }
-        return isPal;
     }
 
     public static void main(String[] args) {
@@ -36,7 +26,5 @@ public class IntegerPalindrom {
         System.out.println(intPal.isPalindrom(12121));
         System.out.println(intPal.isPalindrom(1212));
 
-        System.out.println(IntegerPalindrom.isIntegerPalindromReversed(98989));
-        System.out.println(IntegerPalindrom.isIntegerPalindromReversed(9898977));
     }
 }
--- src/com/company/sorting/BubbleSorting.java
+++ src/com/company/sorting/BubbleSorting.java
@@ -44,7 +44,9 @@ public class BubbleSorting {
     }
 
     public static void main(String[] args) {
+
         int[] tabs = BubbleSorting.sort(new int[]{1, 4, 5, 6, 8, 2, 3, 5});
+
         for (int element : tabs) {
             System.out.println(element);
         }
