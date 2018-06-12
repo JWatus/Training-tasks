005d5b9c1a59a8932b4c07e2c7eadd63d9054348
Kuba <kwatus@gmail.com>
2018-06-12 23:11:08 +0200

    Refactored selection sort class

--- src/com/company/sorting/SelectionSort.java
+++ src/com/company/sorting/SelectionSort.java
@@ -42,10 +42,10 @@ public class SelectionSort {
         for (int i = 0; i < n - 1; i++) {
             // Find the minimum element in unsorted array
             int min_idx = i;
-            for (int j = i + 1; j < n; j++)
+            for (int j = i + 1; j < n; j++) {
                 if (arr[j] < arr[min_idx])
                     min_idx = j;
-
+            }
             // Swap the found minimum element with the first
             // element
             int temp = arr[min_idx];
