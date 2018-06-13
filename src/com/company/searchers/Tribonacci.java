c9a5f8e50db2d577dd634e923e3f7466bb18c8c2
Jakub Watus <jwatus@pl.sii.eu>
2018-06-13 11:52:50 +0200

    Tribonacci task added

---
+++ src/com/company/searchers/Tribonacci.java
@@ -0,0 +1,19 @@
+package com.company.searchers;
+
+public class Tribonacci {
+
+    public double[] tribonacci(double[] s, int n) {
+        double[] res = new double[n];
+        if (n < 3) {
+            for (int i = 0; i < n; i++) res[i] = s[i];
+            return res;
+        }
+        for (int i = 0; i < 3; i++) {
+            res[i] = s[i];
+        }
+        for (int i = 3; i < n; i++) {
+            res[i] = res[i - 1] + res[i - 2] + res[i - 3];
+        }
+        return res;
+    }
+}
