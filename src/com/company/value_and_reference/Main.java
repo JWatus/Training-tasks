bf2f2d4700f1fd83dd670abb336cc910cf0b0f72
Kuba <kwatus@gmail.com>
2018-07-07 15:03:15 +0200

    Autoboxing and wrappers training

--- src/com/company/value_and_reference/Main.java
+++ src/com/company/value_and_reference/Main.java
@@ -9,14 +9,16 @@ public class Main {
         System.out.println(a);
         System.out.println(b);
         System.out.println("............");
-        System.out.println(switcherObjectImmutable(a,b));
+        System.out.println(switcherObjectImmutable(a, b));
 
-        Car car = new Car(20,"Mercedes");
+        Car car = new Car(20, "Mercedes");
         Car car1 = car;
         System.out.println(car.getName());
         switcherObjectMutable(car);
         System.out.println(car.getName());
         System.out.println(car1.getName());     // car1 rowniez sie zmieni mimo ze modyfikuje tylko car
+
+        wrapp();
     }
 
     private static Integer switcherObjectImmutable(Integer a, Integer b) {
@@ -28,5 +30,29 @@ public class Main {
 
     private static void switcherObjectMutable(Car car) {
         car.setName("Polonez");
+    }
+
+    private static void wrapp() {
+
+        System.out.println("\n\n-.-.-.-.-.-.-.-.-.-");
+
+        String thisString = "103";
+        System.out.println("\nthisString: " + thisString);
+
+        int i = 5;
+        System.out.println("\nint i: " + i);
+
+        Integer integer = Integer.valueOf(i);
+        System.out.println("\ninteger value: " + integer);
+
+        Integer strinteger = Integer.valueOf(thisString);
+        System.out.println("\nstrinteger value: " + strinteger);
+
+        Integer integer2 = integer;
+        System.out.println("\ninteger 2 value: "+integer2);
+
+        integer = 89;
+        System.out.println("\ninteger actual value: "+integer);
+        System.out.println("integer 2 actual value: "+integer2);
     }
 }
